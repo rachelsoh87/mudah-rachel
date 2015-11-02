1e03922a2a90a449c92f9e2b9a5d0393acae6078
nextacademy <nextacademy@email.com>
2015-11-02 14:16:19 +0800

    added editing logic, forms and page

--- app/controllers/products.rb
+++ app/controllers/products.rb
@@ -12,12 +12,67 @@ post "/products" do
 	end
 end
 
-# get "/products/:product_id" do
-#   if logged_in?
-#     @message = "Your product has been successfully posted!"
-#     erb :"homepage"
-#   else
-#     @message = "Please log-in!"
-#     erb :"homepage"
-#   end
-# end
+get "/products/:product_id" do
+  @product = Product.find(params[:product_id])
+  @product_poster = User.find(@product.user_id)
+
+  if logged_in?
+    erb :"individual_product"
+  else
+    @message = "Please log-in!"
+    erb :"homepage"
+  end
+end
+
+
+get "/products/:product_id/edit" do
+  @product = Product.find(params[:product_id])
+  @product_poster = User.find(@product.user_id)
+  if logged_in?
+    if current_user == Product.find(params[:product_id]).user
+      erb :"edit_product"
+    else
+      @message = "Sorry you're not authorized to edit this product!"
+      erb :"individual_product"
+    end
+  else
+    @message = "Please log-in!"
+    erb :"homepage"
+  end
+end
+
+
+patch "/products/:product_id/edit" do
+  @product = Product.find(params[:product_id])
+  @product_poster = User.find(@product.user_id)
+  if logged_in?
+    @product.update(description: params[:product][:description])
+    @message = "Product successfully edited!"
+  erb :"individual_product"
+  else
+    @message = "Please log-in!"
+    erb :"homepage"
+  end
+end
+
+delete "/products/:product_id/delete" do
+  @product = Product.find(params[:product_id])
+  @product_poster = User.find(@product.user_id)
+  if logged_in?
+    #if current user is the same as product uploader, allow that individual to edit
+    if current_user == Product.find(params[:product_id]).user 
+      @product.destroy
+      @message = "Product successfully deleted!"
+      erb :"homepage"
+    else
+      @message = "Sorry you're not authorized to edit this question!"
+      erb :"individual_product"
+    end
+  else
+    @message = "Please log-in!"
+    erb :"homepage"
+  end
+end
+
+
+
